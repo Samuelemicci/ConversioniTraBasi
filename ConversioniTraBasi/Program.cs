0303d3ddba2a152c2643b670bafe1579936ec7c1
samu180604@gmail.com <samu180604@gmail.com>
2020-10-17 09:39:27 +0200

    Aggiungere i file di progetto.

---
+++ ConversioniTraBasi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConversioniTraBasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
